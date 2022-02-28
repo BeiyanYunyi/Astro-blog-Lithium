802487ce35c49ec139ac445396d4b1251fb5dcce
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-02-28 11:18:11 +0800

    💄 Styling for friends

--- src/Friends.vue
+++ src/Friends.vue
@@ -1,24 +1,20 @@
 <template>
-  <n-list>
+  <n-space vertical>
     <n-a v-for="friend in friends" :href="friend.url">
-      <n-list-item>
-        <template #prefix>
-          <n-thing></n-thing>
-        </template>
+      <n-card hoverable embedded content-style="padding: 0.25rem 0.5rem 0.25rem 0.5rem">
         <n-thing :title="friend.title" :description="friend.description">
           <template #avatar>
-            <n-image height="100" width="100" :src="friend.avatar" :preview-disabled="true" />
+            <n-image height="75" width="75" :src="friend.avatar" :preview-disabled="true" />
           </template>
         </n-thing>
-      </n-list-item>
+      </n-card>
     </n-a>
-  </n-list>
+  </n-space>
 </template>
 
 <script lang="ts" setup>
 import friends from '../docs/_data/friends.json';
-import { NImage, NList, NListItem, NThing,NA } from 'naive-ui';
+import { NImage, NSpace, NThing, NA, NCard } from 'naive-ui';
 </script>
 
-<style module>
-</style>
+<style module></style>
