4f37206c2cad39ac252cff8731b2aaaa92e2f5f4
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-02-28 12:46:00 +0800

    💄 Dark mode for friends

--- src/Friends.vue
+++ src/Friends.vue
@@ -1,8 +1,16 @@
 <template>
   <n-space vertical>
     <n-a v-for="friend in friends" :href="friend.url">
-      <n-card hoverable embedded content-style="padding: 0.25rem 0.5rem 0.25rem 0.5rem">
-        <n-thing :title="friend.title" :description="friend.description">
+      <n-card
+        hoverable
+        embedded
+        :class="$style.card"
+        content-style="padding: 0.25rem 0.5rem 0.25rem 0.5rem"
+      >
+        <n-thing :class="$style.content" :description="friend.description">
+          <template #header>
+            <div :class="$style.title">{{ friend.title }}</div>
+          </template>
           <template #avatar>
             <n-image height="75" width="75" :src="friend.avatar" :preview-disabled="true" />
           </template>
@@ -17,4 +25,18 @@ import friends from '../docs/_data/friends.json';
 import { NImage, NSpace, NThing, NA, NCard } from 'naive-ui';
 </script>
 
-<style module></style>
+<style module>
+.card {
+  background-color: var(--bg-color-blur);
+}
+.content {
+  color: var(--text-color);
+}
+
+.title {
+  font-size: 16px;
+  font-weight: var(--n-title-font-weight);
+  transition: color 0.3s var(--n-bezier);
+  color: var(--text-color-bright);
+}
+</style>
