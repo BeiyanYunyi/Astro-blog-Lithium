8ea5453c7cd80ff4c3679720d8575d4c5b8337bb
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-03-25 16:06:44 +0800

    🔥 Remove naive-ui

--- src/Friends.vue
+++ src/Friends.vue
@@ -1,36 +1,51 @@
 <template>
-  <n-space vertical>
-    <n-a v-for="friend in friends" :href="friend.url">
-      <n-card
-        hoverable
-        embedded
-        :class="$style.card"
-        content-style="padding: 0.25rem 0.5rem 0.25rem 0.5rem"
-      >
-        <n-thing :class="$style.content" :description="friend.description">
-          <template #header>
+  <div :class="$style.stack">
+    <a
+      v-for="(friend, index) in friends"
+      :class="{ [$style.firstElement]: index === 0 }"
+      :key="index"
+      :href="friend.url"
+    >
+      <div :class="$style.card">
+        <div :class="$style.cardLayer1">
+          <img height="75" width="75" :src="friend.avatar" :class="$style.avatar" />
+          <div :class="$style.cardLayer2">
             <div :class="$style.title">{{ friend.title }}</div>
-          </template>
-          <template #avatar>
-            <n-image height="75" width="75" :src="friend.avatar" :preview-disabled="true" />
-          </template>
-        </n-thing>
-      </n-card>
-    </n-a>
-  </n-space>
+            <div :class="$style.content">{{ friend.description }}</div>
+          </div>
+        </div>
+      </div>
+    </a>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import friends from '../docs/_data/friends.json';
-import { NImage, NSpace, NThing, NA, NCard } from 'naive-ui';
 </script>
 
 <style module>
 .card {
   background-color: var(--bg-color-blur);
+  transition: all 0.25s;
+  padding: 0.5rem 0.5rem 0.25rem 0.5rem;
+  outline: 1px solid var(--border-color);
+  overflow-wrap: break-word;
+}
+.card:hover {
+  box-shadow: 0 1px 3px 1px var(--card-shadow);
+  text-decoration: underline;
+}
+
+.card:hover .title{
+  text-decoration: underline;
+}
+.card:hover .content{
+  text-decoration: underline;
 }
 .content {
   color: var(--text-color);
+  margin-top: 0.25rem;
+  font-size: 14px;
 }
 
 .title {
@@ -38,5 +53,31 @@ import { NImage, NSpace, NThing, NA, NCard } from 'naive-ui';
   font-weight: var(--n-title-font-weight);
   transition: color 0.3s var(--n-bezier);
   color: var(--theme-color);
+}
+
+.stack {
+  display: flex;
+  flex-direction: column;
+}
+.stack a {
+  margin-top: 0.5rem;
+}
+
+.stack a.firstElement {
+  margin-top: 0;
+}
+
+.cardLayer1 {
+  display: inline-flex;
+  flex-direction: row;
+}
+
+.cardLayer2 {
+  display: inline-flex;
+  flex-direction: column;
+}
+
+.avatar {
+  margin-right: 0.75rem;
 }
 </style>
