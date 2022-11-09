8b0aaee20338b2c9c9cb5a7f435b751f59c69692
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-11-09 22:10:55 +0800

    ✨ New comment and PV

--- src/utils/getWordCount.ts
+++ src/utils/getWordCount.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs';
 import duration from 'dayjs/plugin/duration';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/zh-cn';
 
 dayjs.extend(duration);
 dayjs.extend(relativeTime);
