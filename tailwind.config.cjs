978f1e078c4258ae045939f01eea80f685950ac0
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-12-19 18:21:07 +0800

    📝 Add an article

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,18 @@ module.exports = {
         app: '0 4px 10px rgba(0, 0, 0, 0.05), 0 0 1px rgba(0, 0, 0, 0.1)',
         appDark: '0 4px 10px rgba(255, 255, 255, 0.05), 0 0 1px rgba(255, 255, 255, 0.1)',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            blockquote: {
+              'font-style': 'initial',
+              // 在找到对暗黑模式友好的方式以前保持注释
+              // 'background-color': colorPalette.majorBright,
+              'margin-inline': '0',
+            },
+          }
+        }
+      },
     },
   },
   darkMode: 'class',
