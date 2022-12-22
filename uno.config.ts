d7afca0d843ba221f7ea0f79cb84beafeb6adc73
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-12-23 03:03:27 +0800

    🎨

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,16 @@ import transformerDirectives from '@unocss/transformer-directives';
 import { defineConfig, presetTypography, presetWind, presetAttributify } from 'unocss';
 
 export default defineConfig({
-  presets: [presetWind(), presetAttributify(), presetTypography()],
+  presets: [
+    presetWind(),
+    presetAttributify(),
+    presetTypography({
+      cssExtend: {
+        'blockquote:before, blockquote:after, code:before, code:after': { content: 'none' },
+        p: { 'white-space': 'pre-line' },
+      },
+    }),
+  ],
   transformers: [transformerDirectives()],
   rules: [
     ['shadow-app', { 'box-shadow': '0 4px 10px rgba(0, 0, 0, 0.05), 0 0 1px rgba(0, 0, 0, 0.1)' }],
