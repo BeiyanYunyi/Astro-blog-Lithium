0aebc92a38f2fa60b695a5f44b4631894a5e0efa
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-12-22 18:08:07 +0800

    📝 Modify removeHexo.mdx

--- src/components/ShowCase.tsx
+++ src/components/ShowCase.tsx
@@ -5,7 +5,13 @@ const ShowCase: Component = () => {
   const [count, setCount] = createSignal(0);
   return (
     <div class="card p-4">
-      <p>例如，这是一个使用 solid.js 实现的计数器，而我在博文里只用一行代码就引入了它。你可以点击下面的两个按钮查看效果。</p>
+      <p>
+        例如，这是一个使用 solid.js 实现的计数器，而我在博文里
+        <a target='blank' href="https://github.com/lixiang810/comb/blob/main/src/pages/posts/removeHexo.mdx?plain=1#L10">
+          只用一行代码
+        </a>
+        就引入了它。你可以点击下面的两个按钮查看效果。
+      </p>
       <p class="text-center text-lg">{count()}s</p>
       <div class="flex gap-2 justify-center">
         <button
