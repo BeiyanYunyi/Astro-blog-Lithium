59bac197940ad78d417abd13e5b1e26b8700aef9
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-12-24 02:05:15 +0800

    💄 Styling

--- uno.config.ts
+++ uno.config.ts
@@ -20,13 +20,7 @@ export default defineConfig({
   transformers: [transformerDirectives()],
   rules: [
     ['shadow-app', { 'box-shadow': '0 4px 10px rgba(0, 0, 0, 0.05), 0 0 1px rgba(0, 0, 0, 0.1)' }],
-    [
-      'shadow-appDark',
-      {
-        'box-shadow': '0 4px 10px rgba(255, 255, 255, 0.05), 0 0 1px rgba(255, 255, 255, 0.1)',
-      },
-    ],
   ],
-  shortcuts: { card: 'bg-white dark:bg-stone-800 rounded-md shadow-app dark:shadow-appDark' },
+  shortcuts: { card: 'bg-white dark:bg-stone-800 rounded-md shadow-app' },
   layers: { shortcuts: -1 },
 });
