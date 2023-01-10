43db89aab41792240d4594a59418add1298e2209
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2023-01-10 22:04:07 +0800

    🎨

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,8 +2,8 @@ import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '@config';
 import type MDInstance from '@app-types/MDInstance';
 
-const importResults = import.meta.glob('./posts/*.{md,mdx}', { eager: true });
-const posts = (Object.values(importResults) as MDInstance[]).map((item) => ({
+const importResults = import.meta.glob<MDInstance>('./posts/*.{md,mdx}', { eager: true });
+const posts = Object.values(importResults).map((item) => ({
   link: item.url!,
   title: item.frontmatter.title,
   pubDate: new Date(item.frontmatter.date),
