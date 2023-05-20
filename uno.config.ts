1eb75dbc186519dff5fdbe91d22bbb85308ed3b2
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-05-20 18:38:57 +0800

    💄

--- uno.config.ts
+++ uno.config.ts
@@ -27,6 +27,7 @@ export default defineConfig({
   transformers: [transformerDirectives(), transformerVariantGroup()],
   rules: [
     ['shadow-app', { 'box-shadow': '0 4px 10px rgba(0, 0, 0, 0.05), 0 0 1px rgba(0, 0, 0, 0.1)' }],
+    ['pre-wrap', { 'white-space': 'pre-wrap' }],
   ],
   shortcuts: { card: 'bg-white dark:bg-stone-800 rounded-md shadow-app' },
   layers: { shortcuts: -1 },
