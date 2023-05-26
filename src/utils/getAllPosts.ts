7a0e996e13d09dc81bfad27a16b2dd9b0fffa882
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-05-26 14:54:10 +0800

    ⚡️ Add lock for getAllPosts

--- src/utils/getAllPosts.ts
+++ src/utils/getAllPosts.ts
@@ -1,11 +1,13 @@
 import type MDInstance from '@app-types/MDInstance';
 
 let allPosts: MDInstance[] = [];
+let lock = false;
 
 const getAllPosts = async () => {
   const posts = import.meta.glob<MDInstance>('../pages/posts/*.{md,mdx}');
 
   if (allPosts.length === 0) {
+    lock = true;
     await Promise.all(
       Object.values(posts).map(async (iterator, index) => {
         if (index === 0) {
@@ -14,14 +16,20 @@ const getAllPosts = async () => {
         allPosts.push(await iterator());
       }),
     );
-  } else {
-    Object.values(posts).map(async (iterator, index) => {
-      const post = await iterator();
-      if (index === 0) {
-        allPosts = [];
-      }
-      allPosts.push(post);
+    lock = false;
+  } else if (!lock) {
+    Promise.all(
+      Object.values(posts).map(async (iterator, index) => {
+        const post = await iterator();
+        if (index === 0) {
+          allPosts = [];
+        }
+        allPosts.push(post);
+      }),
+    ).then(() => {
+      lock = false;
     });
+    lock = true;
   }
   return allPosts;
 };
