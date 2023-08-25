bfb55891fd59be3b7f9630bf569eabb5a769abe4
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-08-26 01:37:57 +0800

    🐛 Disable cache

--- src/utils/getAllPosts.ts
+++ src/utils/getAllPosts.ts
@@ -6,8 +6,8 @@ let lock = false;
 const getAllPosts = async () => {
   const posts = import.meta.glob<MDInstance>('../pages/posts/*.{md,mdx}');
 
-  if (allPosts.length === 0) {
-    lock = true;
+  //if (allPosts.length === 0) {
+  //  lock = true;
     await Promise.all(
       Object.values(posts).map(async (iterator, index) => {
         if (index === 0) {
@@ -16,21 +16,21 @@ const getAllPosts = async () => {
         allPosts.push(await iterator());
       }),
     );
-    lock = false;
-  } else if (!lock) {
-    Promise.all(
-      Object.values(posts).map(async (iterator, index) => {
-        const post = await iterator();
-        if (index === 0) {
-          allPosts = [];
-        }
-        allPosts.push(post);
-      }),
-    ).then(() => {
-      lock = false;
-    });
-    lock = true;
-  }
+  //  lock = false;
+  //} else if (!lock) {
+  //  Promise.all(
+  //    Object.values(posts).map(async (iterator, index) => {
+  //      const post = await iterator();
+  //      if (index === 0) {
+  //        allPosts = [];
+  //      }
+  //      allPosts.push(post);
+  //    }),
+  //  ).then(() => {
+  //    lock = false;
+  //  });
+  //  lock = true;
+  //}
   return allPosts;
 };
 
