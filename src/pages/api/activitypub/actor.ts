aa41e9486a0891282db28d935c78c103210a14c6
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-10-26 17:06:34 +0800

    🎨 Update actor info

--- src/pages/api/activitypub/actor.ts
+++ src/pages/api/activitypub/actor.ts
@@ -6,15 +6,33 @@ export const GET = () =>
       '@context': ['https://www.w3.org/ns/activitystreams', 'https://w3id.org/security/v1'],
       id: 'https://blog.yunyi.beiyan.us/api/activitypub/actor',
       type: 'Person',
-      name: 'Beiyan Yunyi',
+      name: '北雁云依',
       preferredUsername: 'BeiyanYunyi',
-      summary: 'Blog',
+      summary:
+        '<p>北雁云依的博客<br />拿去吧，你可以假戏真做<br />君子协定：内容默认可自由转发，特别注明除外</p>',
       inbox: 'https://blog.yunyi.beiyan.us/api/activitypub/inbox',
       outbox: 'https://blog.yunyi.beiyan.us/api/activitypub/outbox',
       followers: 'https://blog.yunyi.beiyan.us/api/activitypub/followers',
+      attachment: [
+        { type: 'PropertyValue', name: '立场', value: '泛左翼缝合' },
+        {
+          type: 'PropertyValue',
+          name: '爱好',
+          value: '百科知识。人文社科自然科学计算机科学',
+        },
+        {
+          type: 'PropertyValue',
+          name: '博客',
+          value:
+            '\u003ca href="https://blog.yunyi.beiyan.us" target="_blank" rel="nofollow noopener noreferrer me" translate="no"\u003e\u003cspan class="invisible"\u003ehttps://\u003c/span\u003e\u003cspan class=""\u003eblog.yunyi.beiyan.us\u003c/span\u003e\u003cspan class="invisible"\u003e\u003c/span\u003e\u003c/a\u003e',
+        },
+      ],
       icon: [
         { type: 'Image', mediaType: 'image/png', url: 'https://blog.yunyi.beiyan.us/头像方.png' },
       ],
+      discoverable: true,
+      indexable: true,
+      published: '2018-05-31T00:00:00Z',
       publicKey: {
         id: 'https://blog.yunyi.beiyan.us/api/activitypub/actor#main-key',
         owner: 'https://blog.yunyi.beiyan.us/api/activitypub/actor',
