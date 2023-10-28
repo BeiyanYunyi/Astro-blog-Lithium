f89255ba6365df127248cab32b7b543e98a640cf
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2023-10-28 12:27:50 +0800

    🎨 Make it safer

--- functions/api/sendToInbox.ts
+++ functions/api/sendToInbox.ts
@@ -3,8 +3,11 @@ import { Kysely } from 'kysely';
 import { D1Dialect } from 'kysely-d1';
 import { Database, Env } from '../src/types';
 import AppRequest from '../src/utils/AppRequest';
+import { getPrivateKey } from '../src/utils/getKey';
 
-export const onRequestGet: PagesFunction<Env> = async (ctx) => {
+export const onRequestPost: PagesFunction<Env> = async (ctx) => {
+  const body = await ctx.request.json();
+  if (body !== getPrivateKey(ctx.env)) return new Response('Bad Request', { status: 400 });
   const db = new Kysely<Database>({ dialect: new D1Dialect({ database: ctx.env.ap }) });
   const follower = await db
     .selectFrom('follower')
@@ -13,13 +16,12 @@ export const onRequestGet: PagesFunction<Env> = async (ctx) => {
     .executeTakeFirst();
   const res = await ctx.env.ASSETS.fetch('https://blog.yunyi.beiyan.us/api/activitypub/outbox');
   const json: { orderedItems: unknown[] } = await res.json();
-  const reqs = json.orderedItems.map(
-    (item) => new AppRequest(follower.inbox, { body: JSON.stringify(item) }),
-  );
+  const reqs = json.orderedItems
+    .map((item) => new AppRequest(follower.inbox, { body: JSON.stringify(item) }))
+    .reverse();
   for await (const req of reqs) {
     await req.digestAndSign(ctx.env);
-    const es = await fetch(req);
-    console.log(await es.text(), es.status);
+    await fetch(req);
   }
   return new Response(JSON.stringify(json.orderedItems), {
     headers: { 'Content-Type': 'application/activity+json' },
