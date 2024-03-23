76cc4081cb49b5a7be36de412cf50bc4adce4cf5
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-03-23 11:31:38 +0800

    chore: :arrow_up: Upgrade dependencies

--- src/utils/shuffle.ts
+++ src/utils/shuffle.ts
@@ -1,6 +1,6 @@
 const shuffle = <T>(arr: T[]) => {
   let l = arr.length;
-  const brr = arr;
+  const brr = [...arr];
   while (l > 0) {
     const index = Math.floor(Math.random() * l);
     const temp = brr[l - 1];
