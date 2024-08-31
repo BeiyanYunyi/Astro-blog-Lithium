bc79bbcf6bdc30fcc4c74559c71eef7b118b29b3
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2024-08-31 17:02:09 +0800

    feat: :sparkles: Add cover for SexualAssaultNote.md

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,12 +2,14 @@ import { defineCollection, z } from 'astro:content';
 
 const postsCollection = defineCollection({
   type: 'content',
-  schema: z.object({
-    title: z.string(),
-    date: z.date(),
-    description: z.string(),
-    tag: z.array(z.string()).optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.date(),
+      description: z.string(),
+      tag: z.array(z.string()).optional(),
+      image: image().optional(),
+    }),
 });
 
 export const collections = { posts: postsCollection };
