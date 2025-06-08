97ae0d0859e08e8da9b063c404fe7257f77ecdb1
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-06-08 15:00:50 +0800

    chore(utils): :art: add a stalker's ua

--- src/utils/blockStalker.ts
+++ src/utils/blockStalker.ts
@@ -1,5 +1,6 @@
 const uaList = [
   'Mozilla/5.0 (Linux; Android 9; EML-AL00; HMSCore 6.15.0.301; GMSCore 14.7.99) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.196 HuaweiBrowser/16.0.3.30036 (KHTML, like Gecko) Chrome/114.0.5735.196 HuaweiBrowser/16.0.3.300 Mobile Safari/537.36',
+  'Mozilla/5.0 (iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15(iPhone; CPU iPhone OS 15_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.2 Mobile/15E148 Safari/604.1',
 ];
 if (uaList.includes(navigator.userAgent)) {
   alert(
