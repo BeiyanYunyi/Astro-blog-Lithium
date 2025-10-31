dd34c1a63cf433b4f829570d9ea69d25c5f91255
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2025-10-31 22:35:59 +0800

    fix: 🐛 fix DarkSwitch

--- src/components/DarkSwitch.tsx
+++ src/components/DarkSwitch.tsx
@@ -1,45 +1,72 @@
-import { type Component, createEffect, createRoot, createSignal, Show } from 'solid-js';
+import type { Component } from 'solid-js'
+import { makePersisted } from '@solid-primitives/storage'
+import { createEffect, createRoot, createSignal, Show } from 'solid-js'
 
-const createDarkMode = () => {
-  const [dark, setDark] = createSignal(window.matchMedia('(prefers-color-scheme: dark)').matches);
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-    setDark(e.matches);
-  });
+function createDarkMode() {
+  const [darkMode, setDarkMode] = makePersisted(
+    // eslint-disable-next-line solid/reactivity
+    createSignal<'auto' | 'light' | 'dark'>('auto'),
+    { storage: localStorage, name: 'dark-mode' },
+  )
+  const [systemDark, setSystemDark] = createSignal(
+    window.matchMedia('(prefers-color-scheme: dark)').matches,
+  )
+  const dark = () =>
+    darkMode() === 'auto' ? systemDark() : darkMode() === 'dark'
+
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', (e) => {
+      if (darkMode() === 'auto')
+        setSystemDark(e.matches)
+    })
+
+  const refreshDark = () => {
+    if (dark()) {
+      window.document.documentElement.classList.add('dark')
+    }
+    else {
+      window.document.documentElement.classList.remove('dark')
+    }
+  }
   createEffect(() => {
     if (dark()) {
-      window.document.documentElement.classList.add('dark');
-    } else {
-      window.document.documentElement.classList.remove('dark');
+      window.document.documentElement.classList.add('dark')
     }
-  });
-  return { dark, setDark };
-};
+    else {
+      window.document.documentElement.classList.remove('dark')
+    }
+  })
+  document.addEventListener('astro:page-load', refreshDark)
+  return { darkMode, setDarkMode, systemDark, refreshDark, dark }
+}
 
-export const darkMode = createRoot(createDarkMode);
+export const darkMode = createRoot(createDarkMode)
 
 const DarkSwitch: Component = () => (
   <Show
-    when={darkMode.dark()}
-    fallback={
+    when={darkMode.darkMode() === 'auto'}
+    fallback={(
+
       <button
         onClick={(e) => {
-          e.preventDefault();
-          darkMode.setDark(true);
+          e.preventDefault()
+          darkMode.setDarkMode('auto')
         }}
         aria-label="Turn on dark mode"
-        class="i-ion:moon-outline text-2xl ml-2"
+        class={`ml-2 text-2xl ${darkMode.dark() ? 'i-ion:moon-outline' : 'i-ion:sunny-outline'}`}
       />
-    }
+    )}
   >
     <button
       onClick={(e) => {
-        e.preventDefault();
-        darkMode.setDark(false);
+        e.preventDefault()
+        darkMode.setDarkMode(darkMode.systemDark() ? 'light' : 'dark')
       }}
       aria-label="Turn off dark mode"
-      class="i-ion:sunny-outline text-2xl ml-2"
+      class="i-ion:transgender-outline ml-2 text-2xl"
     />
   </Show>
-);
+)
 
-export default DarkSwitch;
+export default DarkSwitch
