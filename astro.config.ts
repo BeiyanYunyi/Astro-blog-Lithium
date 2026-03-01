f26d4f4df2a2b617bdc22f111806080beb5c54de
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2026-03-01 17:00:13 +0800

    docs(posts): 📝 add CoastalUniverse.md

--- astro.config.ts
+++ astro.config.ts
@@ -1,11 +1,11 @@
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import solidJs from '@astrojs/solid-js';
-import { defineConfig } from 'astro/config';
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
-import UnoCSS from 'unocss/astro';
-import injectDefaultLayout from './src/utils/injectDefaultLayouts';
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import solidJs from '@astrojs/solid-js'
+import { defineConfig } from 'astro/config'
+import rehypeKatex from 'rehype-katex'
+import remarkMath from 'remark-math'
+import UnoCSS from 'unocss/astro'
+import injectDefaultLayout from './src/utils/injectDefaultLayouts'
 
 // https://astro.build/config
 export default defineConfig({
@@ -17,4 +17,5 @@ export default defineConfig({
     rehypePlugins: [rehypeKatex],
     gfm: true,
   },
-});
+  image: { domains: ['s3.penclub.club'] },
+})
--- src/pages/og-image/[...slug].png.ts
+++ src/pages/og-image/[...slug].png.ts
@@ -1,21 +1,22 @@
-import svgPathToName from '@utils/svgPathToName';
-import type { APIRoute, GetStaticPaths } from 'astro';
-import { getCollection, type CollectionEntry } from 'astro:content';
-import sharp from 'sharp';
+import type { APIRoute, GetStaticPaths } from "astro";
+import type { CollectionEntry } from "astro:content";
+import svgPathToName from "@utils/svgPathToName";
+import { getCollection } from "astro:content";
+import sharp from "sharp";
 
 export const getStaticPaths = (async () => {
-  const blogEntries = await getCollection('posts');
+  const blogEntries = await getCollection("posts");
   return blogEntries
-    .filter((i) => !!i.data.image)
+    .filter((i) => !!i.data.image && !!i.data.image.src)
     .map((entry) => ({
       params: { slug: svgPathToName(entry.data.image!.src) },
       props: entry,
     }));
 }) satisfies GetStaticPaths;
 
-export const GET: APIRoute<CollectionEntry<'posts'>> = async ({ props }) => {
+export const GET: APIRoute<CollectionEntry<"posts">> = async ({ props }) => {
   const image = await sharp(`./dist${props.data.image!.src}`)
-    .resize(1200, 630, { fit: 'contain' })
+    .resize(1200, 630, { fit: "contain" })
     .png()
     .toBuffer();
   return new Response(image.buffer);
