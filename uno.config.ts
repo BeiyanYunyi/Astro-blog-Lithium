ab3a175de639f60982d61b6d8d75ea8a77d1361e
BeiyanYunyi <14120445+BeiyanYunyi@users.noreply.github.com>
2026-06-21 21:24:54 +0800

    docs: 📝 update DrugHistoryofPRC.md

--- uno.config.ts
+++ uno.config.ts
@@ -36,6 +36,7 @@ if (useMiSans)
   sansFont.unshift('MiSans')
 
 export default defineConfig({
+  content: { filesystem: ['src/**/*.md'] },
   presets: [
     presetWind3(),
     presetAttributify(),
